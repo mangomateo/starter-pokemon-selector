3b14cb9594be7fedf66aac2ce6d2ff5d4c446314
Matthew Manzano <velarde.matthew@gmail.com>
2020-05-04 00:26:51 -0400

    - added button functionality - time spent: 1 pomodoro

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-
+// Pokemon Images
 let pkmn1 = document.getElementById('bulbasaur');
 let pkmn2 = document.getElementById('charmander');
 let pkmn3 = document.getElementById('squirtle');
+
+// Pokemon Descriptions
+let pkmnDesc1 = document.getElementById('bulb-txt');
+let pkmnDesc2 = document.getElementById('charm-txt');
+let pkmnDesc3 = document.getElementById('squirt-txt');
+
+// Buttons
+let btn1 = document.getElementById('btn-bulb');
+let btn2 = document.getElementById('btn-charm');
+let btn3 = document.getElementById('btn-squirt');
 
 
 // Color Change Effects
@@ -39,4 +49,14 @@ pkmn2.addEventListener('click', function() {
 
 pkmn3.addEventListener('click', function() {
   confirm('You want to choose the water Pokemon, Squirtle?');
+});
+
+
+// Button Functions
+btn1.addEventListener('click', function() {
+  if (pkmnDesc1.style.display = 'none') {
+    pkmnDesc1.style.display = 'block';
+  } else {
+    pkmnDesc1.style.display = 'none';
+  }
 });
